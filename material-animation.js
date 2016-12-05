4f317c8e60e5f86b0e2a1d4f31c2a849146b098c
David Bielik <dbielik90@gmail.com>
2016-12-05 13:19:09 -0500

    update class add/remove logic

--- material-animation.js
+++ material-animation.js
@@ -24,10 +24,21 @@
             var multiplier = 0.07;
             for (var i = 0; i < count; i++) {
                 delay = (i * multiplier);
+                angular.element(elements[i]).addClass('material-animation material-animation__animating');
                 elements[i].style.webkitTransitionDelay = delay + 's';
                 elements[i].style.transitionDelay = delay + 's';
-                elements[i].className += ' material-animation material-animation__animating';
+                removeAnimationClasses(elements[i], delay);
             }
+        }
+
+        function removeAnimationClasses(node, delay){
+            var element = angular.element(node);
+            var classToRemove = 'material-animation__animating';
+            var classToAdd = 'material-animation__animated';
+            setTimeout(function(){
+                element.removeClass(classToRemove);
+                element.addClass(classToAdd);
+            }, delay * 1e3);
         }
 
         function getVisibleElementsCount(elements){
@@ -42,9 +53,10 @@
         }
 
         function showRemainingElements(elements) {
+            var element;
             for (var i = 0, l = elements.length; i < l; i++) {
-                var child = elements[i];
-                child.className += ' material-animation material-animation__animating';
+                element = angular.element(elements[i]);
+                element.addClass('material-animation material-animation__animated');
             }
         }
 })();
